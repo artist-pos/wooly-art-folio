b166cbee9ae8519d9f8fa3b239816d449d53aff6
artist-pos <pos.artist@gmail.com>
2025-11-05 01:20:32 +1300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>  // Swap here
+      <HashRouter>  
   <Routes>
     <Route path="/" element={<Index />} />
     <Route path="*" element={<NotFound />} />
